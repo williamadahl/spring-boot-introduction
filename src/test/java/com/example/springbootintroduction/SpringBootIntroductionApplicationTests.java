981022fc5ba5de5472faba555308a4150ff999d0
William Dahl <williamad90@gmail.com>
2021-02-18 15:31:45 +0100

    set up starter project

---
+++ src/test/java/com/example/springbootintroduction/SpringBootIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
