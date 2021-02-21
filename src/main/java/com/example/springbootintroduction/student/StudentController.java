1b04fef870f1ba8c7bce358bd2a06ac900522f61
William Dahl <williamad90@gmail.com>
2021-02-21 16:08:26 +0100

    set up a potsgres database and connected to it through configs in aplication.properties

--- src/main/java/com/example/springbootintroduction/student/StudentController.java
+++ src/main/java/com/example/springbootintroduction/student/StudentController.java
@@ -12,6 +12,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
--- src/main/java/com/example/springbootintroduction/student/StudentService.java
+++ src/main/java/com/example/springbootintroduction/student/StudentService.java
@@ -1,12 +1,12 @@
 package com.example.springbootintroduction.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-@Component
+@Service
 public class StudentService {
     public List<Student> getStudent(){
         return List.of(
