44735c40fd4687a03807565df165b46a64d817cc
William Dahl <williamad90@gmail.com>
2021-02-21 16:41:48 +0100

    created a StudentCondig which injects objects into the database and a StudentRepository which connects through JPA

---
+++ src/main/java/com/example/springbootintroduction/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.springbootintroduction.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner (StudentRepository repository){
+        return args -> {
+            Student nina = new Student(
+                    "Nina",
+                    "public-anal",
+                    LocalDate.of(2000, FEBRUARY, 5),
+                    21
+
+            );
+            Student miriam = new Student(
+                    "Miriam",
+                    "horse-cock",
+                    LocalDate.of(2005, AUGUST, 5),
+                    21
+            );
+            repository.saveAll(
+                    List.of(nina, miriam)
+            );
+        };
+    }
+}
--- src/main/java/com/example/springbootintroduction/student/StudentService.java
+++ src/main/java/com/example/springbootintroduction/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.springbootintroduction.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,16 +9,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
-    public List<Student> getStudent(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Nina",
-                        "public-anal",
-                        LocalDate.of(2000, Month.FEBRUARY, 5),
-                        21
+    private final StudentRepository studentRepository;
 
-                )
-        );
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudent(){
+
+        return studentRepository.findAll();
     }
 }
