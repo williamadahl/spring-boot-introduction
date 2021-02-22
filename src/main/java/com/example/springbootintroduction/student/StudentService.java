505df60a27ddb3017b7a6323c3daae6f722c111f
William Dahl <williamad90@gmail.com>
2021-02-22 18:52:32 +0100

    added a POST method using Optional in studentRepository and studentService. Also added a exception for not adding students with the same pet

--- src/main/java/com/example/springbootintroduction/student/StudentService.java
+++ src/main/java/com/example/springbootintroduction/student/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -22,6 +23,11 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> studentOptional = studentRepository
+                .findStudentByPet(student.getPet());
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("Pet taken");
+        }
+        studentRepository.save(student);
     }
 }
